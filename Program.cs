0d6d7199076f51c1c4a342e9bf2ea6a32834b0f4
Kamapholoba <St10375885@rcconnect.edu.za>
2024-04-02 14:56:46 +0200

    edited the main program logic in Program.cs to create a user interface for the Recipe Application

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Recipe_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecipeManager recipeManager = new RecipeManager();
+            bool continueProgram = true;
+
+            Console.WriteLine("Welcome to the Recipe Application!");
+
+            while (continueProgram)
+            {
+                Console.WriteLine("===============================================");
+                Console.WriteLine("1. Enter Recipe Details");
+                Console.WriteLine("2. View Recipe");
+                Console.WriteLine("3. Scale Recipe");
+                Console.WriteLine("4. Reset Quantities");
+                Console.WriteLine("5. Clear All Data");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("===============================================");
+
+                Console.Write("Enter your choice: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            recipeManager.EnterRecipeDetails();
+                            break;
+                        case 2:
+                            recipeManager.ViewRecipe();
+                            break;
+                        case 3:
+                            recipeManager.ScaleRecipe();
+                            break;
+                        case 4:
+                            recipeManager.ResetQuantities();
+                            break;
+                        case 5:
+                            recipeManager.ClearData();
+                            break;
+                        case 6:
+                            continueProgram = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/Recipe_App.AssemblyInfo.cs
+++ obj/Debug/net8.0/Recipe_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7e2378fc100991a7152d1669d75c2d4ad500e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490245ababe0483c066549680230f1690e655447")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
